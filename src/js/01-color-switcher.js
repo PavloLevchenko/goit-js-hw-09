ffa8439b00eb80d8b81b74d426dd648036ea7bc4
PavloLevchenko <teasetrnet@gmail.com>
2021-12-15 19:28:56 +0200

    Добавляет первое задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+stopButton.setAttribute('disabled', '');
+let timerId = null;
+
+const switchBodyColor = () => {
+  const color = getRandomHexColor();
+  document.querySelector('body').style = `background-color:${color};`;
+};
+
+const startTimer = () => {
+  startButton.setAttribute('disabled', '');
+  stopButton.removeAttribute('disabled');
+  switchBodyColor();
+  timerId = setInterval(switchBodyColor, 1000);
+};
+
+const stopTimer = () => {
+  clearTimeout(timerId);
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', '');
+};
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
